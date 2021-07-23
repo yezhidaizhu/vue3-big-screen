0665e7d858525ce6e11660ce8c1532015a45b28d
noFish <2580938905@qq.com>
2021-07-23 09:49:27 +0800

    style

--- src/components/Title/index.vue
+++ src/components/Title/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="flex justify-between  h-full items-center">
-    <DvDecoration8 style="width:300px;height:50px;" />
-    <DvDecoration7>
-      <span class=" text-3xl font-bold p-2">
-        {{title}}
-      </span>
-    </DvDecoration7>
+  <div class="h-full flex flex-col">
+    <div class="flex justify-between flex-1  items-center">
+      <DvDecoration8 style="width:300px;height:50px;" />
+      <DvDecoration7>
+        <span class=" text-3xl font-bold p-2">
+          {{title}}
+        </span>
+      </DvDecoration7>
 
-    <DvDecoration8 :reverse="true"
-                   style="width:300px;height:50px;" />
+      <DvDecoration8 :reverse="true"
+                     style="width:300px;height:50px;" />
+    </div>
+    <DvDecoration2 :dur='8'
+                   style="width:85%;height:5px;"
+                   class="m-auto" />
   </div>
-  <DvDecoration2 :dur='8'
-                 style="width:85%;height:5px;"
-                 class="m-auto" />
 </template>
 <script setup>
 import { title } from '@/config/index';
